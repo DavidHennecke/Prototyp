8178f0e3e24576d2511d22d53a02cf8fc59cc626
hennecke <david@DESKTOP-3CPGKJN>
2021-06-28 16:02:20 +0200

    Add Namespace Prototyp.Elements and Class Shapefile

---
+++ Prototyp/Elements/Shapefile.cs
@@ -0,0 +1,109 @@
+﻿using OSGeo.GDAL;
+using OSGeo.OGR;
+using OSGeo.OSR;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using Windows.Devices.Geolocation;
+using Windows.UI;
+using Windows.UI.Xaml.Controls.Maps;
+
+namespace Prototyp.Elements
+{
+    public class Shapefile
+    {
+        public string sFilename;
+        private Layer Layer;
+
+        public TreeViewItem InitLayer(string sFilename)
+        {
+            Gdal.SetConfigOption("GDAL_FILENAME_IS_UTF8", "NO");
+            Gdal.SetConfigOption("SHAPE_ENCODING", "");
+            Ogr.RegisterAll();// Register all drivers
+            DataSource ds = Ogr.Open(sFilename, 1); // 0 means read-only, 1 means modifiable
+            if (ds == null)
+            {
+                MessageBox.Show("Failed to open file [{0}]!", sFilename);
+            }
+
+            Layer = ds.GetLayerByIndex(0);
+            if (Layer == null)
+            {
+                MessageBox.Show("Get the {0}th layer failed! n", "0");
+            }
+            string layerName = Layer.GetName();
+            TreeViewItem newChild = new TreeViewItem();
+            newChild.Header = layerName;
+
+            return newChild;
+            
+        }
+
+        public MapPolygon BuildElements()
+        {
+            List<BasicGeoposition> polygonPointList = new List<BasicGeoposition>();
+            MapPolygon polygon = new MapPolygon
+            {
+                ZIndex = 1,
+                FillColor = Colors.Red,
+                StrokeColor = Colors.Blue,
+                StrokeThickness = 3,
+                StrokeDashed = false,
+            };
+            Feature feature = null;
+            do
+            {
+                feature = Layer.GetNextFeature();
+                if (feature != null)
+                {
+                    Geometry geom = feature.GetGeometryRef();
+                    SpatialReference sourceRef = geom.GetSpatialReference();
+                    SpatialReference to_crs = new SpatialReference(null);
+                    to_crs.ImportFromEPSG(4326);
+
+                    if (sourceRef != to_crs)
+                    {
+                        CoordinateTransformation ct = new CoordinateTransformation(sourceRef, to_crs, new CoordinateTransformationOptions());
+
+                        if (geom.Transform(ct) != 0)
+                        {
+                            throw new NotSupportedException("projection failed");
+                        }
+
+                        geom.Transform(ct);
+                    }
+                    Geometry ring = geom.GetGeometryRef(0);
+                    int count = ring.GetPointCount();
+                    
+                    if (count > 0)
+                        for (int i = 0; i < count; i++)
+                        {
+                            polygonPointList.Add(new BasicGeoposition() { Latitude = ring.GetX(i), Longitude = ring.GetY(i) });
+                        }
+                    polygon.Path = new Geopath(polygonPointList);
+                }
+            } while (feature != null);
+            return polygon;
+        }
+
+        //public void ZoomToExtent()
+        //{
+        //    Envelope envelope = new Envelope();
+        //    Layer.GetExtent(envelope, 0);
+        //    Geometry ringEnvelope = new Geometry(wkbGeometryType.wkbLinearRing);
+        //    ringEnvelope.AddPoint_2D(envelope.MinX, envelope.MinY);
+        //    ringEnvelope.AddPoint_2D(envelope.MaxX, envelope.MinY);
+        //    ringEnvelope.AddPoint_2D(envelope.MaxX, envelope.MaxY);
+        //    ringEnvelope.AddPoint_2D(envelope.MinX, envelope.MaxY);
+        //    ringEnvelope.AddPoint_2D(envelope.MinX, envelope.MinY);
+
+        //    Geometry polyEnvelope = new Geometry(wkbGeometryType.wkbPolygon);
+        //    var centeroid = polyEnvelope.Centroid();
+        //}
+
+
+    }
+}
--- Prototyp/MainWindow.xaml.cs
+++ Prototyp/MainWindow.xaml.cs
@@ -13,6 +13,7 @@ using System.Globalization;
 using System.Windows.Controls;
 using System.IO;
 using System.Threading.Tasks;
+using Prototyp.Elements;
 
 namespace Prototyp
 {
@@ -25,108 +26,6 @@ namespace Prototyp
             InitializeComponent();
             GdalBase.ConfigureAll();
 
-        }
-
-        private async void addPolygons(string sFilename)
-        {
-            Gdal.SetConfigOption("GDAL_FILENAME_IS_UTF8", "NO");
-            Gdal.SetConfigOption("SHAPE_ENCODING", "");
-            Ogr.RegisterAll();// Register all drivers
-            DataSource ds = Ogr.Open(sFilename, 1); // 0 means read-only, 1 means modifiable
-            if (ds == null)
-            {
-                MessageBox.Show("Failed to open file [{0}]!", sFilename);
-                return;
-            }
-
-            Layer Layer = ds.GetLayerByIndex(0);
-            if (Layer == null)
-            {
-                MessageBox.Show("Get the {0}th layer failed! n", "0");
-                return;
-            }
-
-            string test = "";
-            string layerName = Layer.GetName();
-
-            TreeViewItem newChild = new TreeViewItem();
-            newChild.Header = layerName;
-            TableOfContentsVector.Items.Add(newChild);
-
-            //Envelope envelope = new Envelope();
-            //Layer.GetExtent(envelope, 0);
-            //Geometry ringEnvelope = new Geometry(wkbGeometryType.wkbLinearRing);
-            //ringEnvelope.AddPoint_2D(envelope.MinX, envelope.MinY);
-            //ringEnvelope.AddPoint_2D(envelope.MaxX, envelope.MinY);
-            //ringEnvelope.AddPoint_2D(envelope.MaxX, envelope.MaxY);
-            //ringEnvelope.AddPoint_2D(envelope.MinX, envelope.MaxY);
-            //ringEnvelope.AddPoint_2D(envelope.MinX, envelope.MinY);
-
-            //Geometry polyEnvelope = new Geometry(wkbGeometryType.wkbPolygon);
-            //var centeroid = polyEnvelope.Centroid();
-            //test += centeroid + Environment.NewLine;
-
-
-            Layer.ResetReading();
-            Feature feature = null;
-            do
-            {
-                feature = Layer.GetNextFeature();
-                await BuildPolygon(feature, test);
-               
-            } while (feature != null);
-
-            txtEditor.Text = test;
-
-        }
-
-        public async Task BuildPolygon(Feature feature, string test)
-        {
-            if (feature != null)
-            {
-                test += "1" + Environment.NewLine;
-                Geometry geom = feature.GetGeometryRef();
-                SpatialReference sourceRef = geom.GetSpatialReference();
-                SpatialReference to_crs = new SpatialReference(null);
-                to_crs.ImportFromEPSG(4326);
-
-                if (sourceRef != to_crs)
-                {
-                    CoordinateTransformation ct = new CoordinateTransformation(sourceRef, to_crs, new CoordinateTransformationOptions());
-
-                    if (geom.Transform(ct) != 0)
-                    {
-                        throw new NotSupportedException("projection failed");
-                    }
-
-                    geom.Transform(ct);
-                }
-                Geometry ring = geom.GetGeometryRef(0);
-                int count = ring.GetPointCount();
-                List<BasicGeoposition> polygonPointList = new List<BasicGeoposition>();
-                if (count > 0)
-                    for (int i = 0; i < count; i++)
-                    {
-                        polygonPointList.Add(new BasicGeoposition() { Latitude = ring.GetX(i), Longitude = ring.GetY(i) });
-
-
-                        // do your processing here
-                    }
-                test += polygonPointList + Environment.NewLine;
-
-                MapPolygon polygon = new MapPolygon
-                {
-                    Path = new Geopath(polygonPointList),
-                    ZIndex = 1,
-                    FillColor = Colors.Red,
-                    StrokeColor = Colors.Blue,
-                    StrokeThickness = 3,
-                    StrokeDashed = false,
-                };
-                map.MapElements.Add(polygon);
-                await Task.Delay(1000);
-
-            };
         }
 
         private void ImportButton_Click(object sender, RoutedEventArgs e)
@@ -147,16 +46,23 @@ namespace Prototyp
 
                     if (ext == ".shp")
                     {
-                        addPolygons(sFilename);
+                        AddShapefile(sFilename);
                     }
-                    
+
                 }
             }
         }
+
+        public void AddShapefile(string sFilename)
+        {
+            Shapefile shapefile = new Shapefile();
+            shapefile.sFilename = sFilename;
+            TreeViewItem newChild = shapefile.InitLayer(shapefile.sFilename);
+            TableOfContentsVector.Items.Add(newChild);
+            MapPolygon polygon = shapefile.BuildElements();
+            map.MapElements.Add(polygon);
+        }
+
+    }
     }
 
-    internal class Test
-    {
-
-    }
-}
